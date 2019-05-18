3615c1ec61cdda6d27cd086cca1a59dc37da048c
Ja-Rek <dto.jarek@gmail.com>
2019-05-18 18:36:58 +0200

    Add feature 'assert not default'

---
+++ Suckless.Asserts.Tests/ExtensionMethods/NotDefautlTest.cs
@@ -0,0 +1,32 @@
+using System;
+using NUnit.Framework;
+using Suckless.Asserts.Tests.Base;
+
+namespace Suckless.Asserts.Tests.ExtensionMethods
+{
+    internal partial class NotDefaultTest : AssertBaseTest<ArgumentException> 
+    {
+        [Test]
+        public void NotDefault_WhenValueIsNotDefaultExpectedValue_DoesNotThrowException()
+        {
+            StubMetadata<int>(1).NotDefault();
+        }
+
+        [TestCase(null), TestCase("AnyName")]
+        public void NotDefault_WhenValueIsNotNotDefaultExpectedValue_ThrowsExceptionWithCorrectMessage(string fieldName)
+        {
+            var messagePart = "that contain 0 cannot be default.";
+
+            AssertExceptionMessage<int>(() => StubMetadata<int>(default, fieldName).NotDefault(), 
+                expecteddName: fieldName,
+                messagePart);
+        }
+
+        [TestCase(null), TestCase("AnyName")]
+        public void NotDefault_WhenValueIsNotNotDefaultExpectedValueAdnCustomMessageWasSpecyfied_ThrowsExceptionWithCorrectMessage(string fieldName)
+        {
+            AssertCustomExceptionMessage(() => StubMetadata<int>(default, fieldName).NotDefault(CUSTOM_MESSAGE), 
+                expected: CUSTOM_MESSAGE);
+        }
+    }
+}

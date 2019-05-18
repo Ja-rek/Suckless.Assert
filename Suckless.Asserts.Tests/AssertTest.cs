01795358a9800093429a893fe05880d5f4e73338
Ja-Rek <dto.jarek@gmail.com>
2019-05-18 12:42:04 +0200

    Add tests

--- Suckless.Asserts.Tests/AssertTest.cs
+++ Suckless.Asserts.Tests/AssertTest.cs
@@ -7,20 +7,40 @@ namespace Suckless.Asserts.Tests
 {
     internal partial class AssertTest
     {
+        const string MESSAGE = "The value: String cannot be null.";
+        const string MESSAGE_WITH_FIELD_NAME = "The value: String cannot be null.";
+
         [Test]
         public void Assert_WhenArgumentIsNull_ThrowsException()
         {
-            string valueStub = null;
+            string value = null;
 
-            Unit.Assert.Throws<ArgumentNullException>(() => Assert(valueStub).Empty());
+            var actualMessage = Unit.Assert.Throws<ArgumentNullException>(() => Assert(value)).Message;
+            var actualMessageWithFieldName = Unit.Assert.Throws<ArgumentNullException>(() => 
+                Assert(value, "value")).Message;
+
+            Unit.Assert.AreEqual(expected: MESSAGE, actualMessage);
+            Unit.Assert.AreEqual(expected: MESSAGE_WITH_FIELD_NAME, actualMessageWithFieldName);
         }
 
         [Test]
         public void Assert_WhenExpressionIsNull_ThrowsException()
         {
-            string valueStub = null;
+            string value = null;
 
-            Unit.Assert.Throws<ArgumentNullException>(() => Assert(() => valueStub).Empty());
+            var actualMessageWithFieldName = Unit.Assert.Throws<ArgumentNullException>(() => 
+                Assert(() => value)).Message;
+
+            Unit.Assert.AreEqual(expected: MESSAGE_WITH_FIELD_NAME, actualMessageWithFieldName);
+        }
+
+        [Test]
+        public void Assert_WhenArgumentIsNotNull_DoesNotThrowsException()
+        {
+            string value = "any value";
+
+            Assert(value);
+            Assert(() => value);
         }
     }
 }
